48a5d38a9349599bb3f7f61aa1ea39c53024ef05
João Amaral <amaral.joao@ua.pt>
2016-11-20 21:09:52 +0000

    Recolher informação basica
    Atraves da API do WorldBank

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*function pais(country) {
+  var nomePais = country.split("-");
+//  $("#nome_pais").html(nomePais[0]);
+  console.log(nomePais[1]);
+  $.ajax(
+    'https://{username}:{password}@api.opsourcecloud.net/oec/0.9/'+nomePais[1]+'',
+    function(data){
+      alert(data);
+    },
+    dataType: 'json'
+  );
+}*/
