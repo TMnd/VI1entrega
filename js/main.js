a0ceeb387b6fcc8349931bf0b902a12bb603ccf4
MRodrigues <r.mafalda@ua.pt>
2016-12-04 23:50:26 +0000

    :boot:
    menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-/*function pais(country) {
-  var nomePais = country.split("-");
-//  $("#nome_pais").html(nomePais[0]);
-  console.log(nomePais[1]);
-  $.ajax(
-    'https://{username}:{password}@api.opsourcecloud.net/oec/0.9/'+nomePais[1]+'',
-    function(data){
-      alert(data);
-    },
-    dataType: 'json'
-  );
-}*/
+$(function () {
+    /* START OF DEMO JS - NOT NEEDED */
+    if (window.location == window.parent.location) {
+        $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+        $('#fullscreen').attr('title', 'Back To Bootsnipp');
+    }
+    $('#fullscreen').on('click', function(event) {
+        event.preventDefault();
+        window.parent.location =  $('#fullscreen').attr('href');
+    });
+    $('#fullscreen').tooltip();
+    /* END DEMO OF JS */
+
+    $('.navbar-toggler').on('click', function(event) {
+		event.preventDefault();
+		$(this).closest('.navbar-minimal').toggleClass('open');
+	})
+});
